1b0c93c67666448ac7aa2f09e8cc65d67339509d
Emerson Santos <89021451+Emersonvol@users.noreply.github.com>
2023-03-30 09:02:01 -0300

    Update javaScript.js

--- wb_incluindo_form/javaScript.js
+++ wb_incluindo_form/javaScript.js
@@ -1,4 +1,28 @@
-function btt(){
-window.alert('Perfil Salvo !!!');
-}
+(function btt(){
+  const alerta = document.querySelector('.btnBotao');
+  alerta.addEventListener('click', e =>{
+  alert('Perfil Salvo !!!');
+  location.reload() 
+  });
+    }())
+   
+
+const quantosMembros = document.querySelector('.qtMenbros')
+quantosMembros.addEventListener("input",(e)=>{
+saida = document.querySelector('#saida')
+saida.innerHTML = `${quantosMembros}`
+saida.innerHTML = quantosMembros.value
+},false)
+
+
+const qtdTrofeus = document.querySelector('#nvTrofeus')
+qtdTrofeus.addEventListener("input", e =>{
+saida2 = document.querySelector('.saida2');
+saida2.innerHTML = qtdTrofeus.value
+},false)
+
+const tagSelecionadas = document.querySelector('input[name="gcla"]:checked').value
+
+
+
 
